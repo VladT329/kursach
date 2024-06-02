73f818415283f913dd0dd89af5f87104dc7619a5
Vladislav <VladT329@gmail.com>
2024-06-03 01:01:06 +0300

    start coding

---
+++ src/Employee.java
@@ -0,0 +1,16 @@
+public class Employee {
+    String surname;
+    String name;
+    String patronymic;
+    String department;
+    int salary;
+
+    public Employee (String surname, String name, String patronymic, String department, int salary){
+        this.surname = surname;
+        this.name = name;
+        this.patronymic = patronymic;
+        this. department = department;
+        this. salary = salary;
+
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,6 @@
 public class Main {
     public static void main(String[] args) {
+        Employee employee1 = new Employee("Иванов", "Иван", "Иванович", "1", 20000);
+        System.out.println("employee1 = " + employee1);
     }
 }
